b8871b9d84aef635ff19602b769122486cb6eb5e
burakylz <burak07y@gmail.com>
2026-06-25 14:41:47 +0100

    Anasayfa interaktif + iletisim/rehberler nav

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,15 +3,17 @@ import unis from './universiteler/universities.json'
 import dil from './dil-okullari/languageschools.json'
 import yaz from './yaz-okullari/summerschools.json'
 import staj from './lise-staj/internships.json'
+import { guides } from './rehberler/guides'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const base = 'https://www.alazedu.com'
-  const top = ['', '/universiteler', '/dil-okullari', '/yaz-okullari', '/lise-staj', '/hakkimizda', '/iletisim']
+  const top = ['', '/universiteler', '/dil-okullari', '/yaz-okullari', '/lise-staj', '/rehberler', '/hakkimizda', '/iletisim']
   const uniRoutes = ((unis as any).universities as any[]).map((u) => `/universiteler/${u.slug}`)
   const dilRoutes = ((dil as any).schools as any[]).map((s) => `/dil-okullari/${s.slug}`)
   const yazRoutes = ((yaz as any).schools as any[]).map((s) => `/yaz-okullari/${s.slug}`)
   const stajRoutes = ((staj as any).programs as any[]).map((s) => `/lise-staj/${s.slug}`)
-  const all = [...top, ...uniRoutes, ...dilRoutes, ...yazRoutes, ...stajRoutes]
+  const rehberRoutes = guides.map((g) => `/rehberler/${g.slug}`)
+  const all = [...top, ...uniRoutes, ...dilRoutes, ...yazRoutes, ...stajRoutes, ...rehberRoutes]
   return all.map((r) => ({
     url: base + r,
     lastModified: new Date(),
