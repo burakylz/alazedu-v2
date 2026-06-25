db2046b01f64012f87469cdafaa916eadec5bbc6
burakylz <burak07y@gmail.com>
2026-06-25 12:35:00 +0100

    Add files via upload

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,13 +1,15 @@
 import type { MetadataRoute } from 'next'
 import unis from './universiteler/universities.json'
 import dil from './dil-okullari/languageschools.json'
+import yaz from './yaz-okullari/summerschools.json'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const base = 'https://www.alazedu.com'
   const top = ['', '/universiteler', '/dil-okullari', '/yaz-okullari', '/hakkimizda', '/iletisim']
   const uniRoutes = ((unis as any).universities as any[]).map((u) => `/universiteler/${u.slug}`)
   const dilRoutes = ((dil as any).schools as any[]).map((s) => `/dil-okullari/${s.slug}`)
-  const all = [...top, ...uniRoutes, ...dilRoutes]
+  const yazRoutes = ((yaz as any).schools as any[]).map((s) => `/yaz-okullari/${s.slug}`)
+  const all = [...top, ...uniRoutes, ...dilRoutes, ...yazRoutes]
   return all.map((r) => ({
     url: base + r,
     lastModified: new Date(),
